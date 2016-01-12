f7e4a796656bfe8efd733c575bb4a719e45f763f
Chau Vo <minhchau273@gmail.com>
2016-01-12 17:11:46 +0700

    Show menu

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,4 +1,5 @@
 class MenuController < ApplicationController
   def index
+  	@menu_items = MenuItem.all
   end
 end
--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,3 +1,11 @@
 class MenuItem < ActiveRecord::Base
   belongs_to :section
+
+  def image_url_or_default
+ 	if image_url.present?
+ 	  image_url
+    else
+      "http://loremflickr.com/320/240/#{CGI.escape name}"
+ 	end
+  end
 end
