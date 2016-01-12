18fde15b80ef89b5d864a78544a4fcdbdedabbce
Chau Vo <minhchau273@gmail.com>
2016-01-12 23:52:11 +0700

    Add Sort feature

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -3,7 +3,13 @@ class MenuController < ApplicationController
     @sections = Section.all
 
     section = Section.find_by_id(params[:section]) || Section.first
-    @menu_items = section.menu_items
+    @sort = params[:sort] || "alphabetical"
+    @menu_items = case @sort
+                  when "alphabetical" then section.menu_items.sort_by { |item| [item.name] }
+                  when "price_low_to_high" then section.menu_items.sort_by { |item| [item.price] }
+                  when "price_high_to_low" then section.menu_items.sort_by { |item| [item.price] }.reverse
+                  else []
+                  end
     @section_id = section.id
   end
 end
