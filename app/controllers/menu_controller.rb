82f1c407d742745abb19159bbbbfb409ca9595e6
Chau Vo <minhchau273@gmail.com>
2016-01-14 11:21:03 +0700

    [r] Change switch-case of @sort

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -5,10 +5,9 @@ class MenuController < ApplicationController
     section = Section.find_or_initialize_by(id: params[:section])
     @sort = params[:sort] || "alphabetical"
     @menu_items = case @sort
-                  when "alphabetical" then section.menu_items.sort_by { |item| [item.name] }
                   when "price_low_to_high" then section.menu_items.sort_by { |item| [item.price] }
-                  when "price_high_to_low" then section.menu_items.sort_by { |item| [item.price] }.reverse
-                  else []
+                  when "price_high_to_low" then section.menu_items.sort_by { |item| [-item.price] }
+                  else section.menu_items.sort_by { |item| [item.name] }
                   end
     @section_id = section.id
   end
