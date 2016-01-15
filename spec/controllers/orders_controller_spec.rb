e9e443b9a0fe4cf5ca853b813f4a77a5586bc80f
Chau Vo <minhchau273@gmail.com>
2016-01-15 11:25:48 +0700

    [f] Add Order spec and Order controller spec

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe OrdersController, type: :controller do
+  let(:item) { create(:menu_item) }
 
+  describe "GET #new" do
+    before do
+      get :new, menu_item_id: item.id
+    end
+
+    it "is success" do
+      expect(assigns(:menu_item)).to eq item
+    end
+  end
+
+  describe "POST #create" do
+    context "successful order" do
+      let(:customerName) { "Cheetah" }
+
+      before do
+        post :create, menu_item_id: item.id, order: { name: customerName }
+      end
+
+      it "redirects to Menu page" do
+        expect(response).to redirect_to menu_path
+        expect(flash["success"]).to eq "Order submitted. Thank you!"
+      end
+    end
+
+    context "unsuccessful order" do
+      before do
+        post :create, menu_item_id: item.id, order: { name: "" }
+      end
+
+      it "goes back to Order page" do
+        expect(response).to render_template :new
+        expect(flash["error"]).to eq "Error: Name can't be blank"
+      end
+    end
+  end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it { is_expected.to belong_to(:menu_item) }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of :menu_item }
+    it { is_expected.to validate_presence_of :name }
+  end
 end
