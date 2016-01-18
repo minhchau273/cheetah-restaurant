89ab10785c6feafc961f3b92daf9dbc1a8080cca
Chau Vo <minhchau273@gmail.com>
2016-01-18 09:54:19 +0700

    [r] Use `order` instead of `sort_by`

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -5,9 +5,9 @@ class MenuController < ApplicationController
     section = Section.find_or_initialize_by(id: params[:section])
     @sort = params[:sort] || "alphabetical"
     @menu_items = case @sort
-                  when "price_low_to_high" then section.menu_items.sort_by { |item| [item.price] }
-                  when "price_high_to_low" then section.menu_items.sort_by { |item| [-item.price] }
-                  else section.menu_items.sort_by { |item| [item.name] }
+                  when "price_low_to_high" then section.menu_items.order(:price)
+                  when "price_high_to_low" then section.menu_items.order(price: :desc )
+                  else section.menu_items.order(:name)
                   end
     @section_id = section.id
   end
